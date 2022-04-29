8593e1cdb58ce6ecf792c37dab30e776a3c5749b
Jack Robinson <jackrobinson920@gmail.com>
2022-04-29 22:50:27 +0100

    Inital experiments with React Native

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableHighlight,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePressed = () => console.log("Clicked");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePressed}>
+        Open up App.js to start working on your app!
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Test"
+        onPress={() =>
+          Alert.alert("Title", "Button Pressed", [
+            {
+              text: "Yes",
+              onPress: () => console.log("Yes"),
+            },
+            {
+              text: "No",
+              onPress: () => console.log("No"),
+            },
+          ])
+        }
+        color="orange"
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(50, 100, 150, 1.0)",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
